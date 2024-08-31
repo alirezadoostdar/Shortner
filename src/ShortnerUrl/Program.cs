f1fddde2d4d2aed217e001820bf8083f9f59e286
alirezadoostdar <alireza.doostdar@gmail.com>
2024-08-31 22:48:48 +0330

    add mongo db setting

--- src/ShortnerUrl/Program.cs
+++ src/ShortnerUrl/Program.cs
@@ -1,10 +1,27 @@
+using Microsoft.EntityFrameworkCore;
 using ShortnerUrl.Endpoints;
+using ShortnerUrl.Infrastructures;
+using ShortnerUrl.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ShortenService>();
+builder.Services.AddDbContext<ShortnerUrlDbContext>(configure =>
+{
+    var host = builder.Configuration["MongoDbConnectionString:Host"];
+    var dbName = builder.Configuration["MongoDbConnectionString:DbName"];
+
+    if(host is null)
+        throw new ArgumentNullException(nameof(host));
+    if(dbName is null)
+        throw new ArgumentNullException(nameof(dbName));
+
+    configure.UseMongoDB(host, dbName);
+});
 
 var app = builder.Build();
 
